9cd1bc237c9172ea6b62bcfb924c1c677afe60a7
suzzingv <84590065+suzzingv@users.noreply.github.com>
2024-06-24 23:34:43 +0900

    [INIT] init setting

---
+++ src/main/java/konkuk/tourkk/chons/BeChonsApplication.java
@@ -0,0 +1,13 @@
+package konkuk.tourkk.chons;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeChonsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BeChonsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/konkuk/tourkk/chons/BeChonsApplicationTests.java
@@ -0,0 +1,13 @@
+package konkuk.tourkk.chons;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeChonsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
