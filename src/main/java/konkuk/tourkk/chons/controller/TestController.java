d022ff372243f2b4979cc5860c4ce972c63e9a02
suzzingv <sjlim1999@naver.com>
2024-06-26 17:25:17 +0900

    [CHORE] test

--- src/main/java/konkuk/tourkk/chons/controller/TestController.java
+++ src/main/java/konkuk/tourkk/chons/controller/TestController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestController {
     @GetMapping("/hello")
     public String hello(){
-        return "Hello #2";
+        return "Hello #3";
     }
 }
