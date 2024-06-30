c707757f2229ba8af10d9823120f7e2c89829e1e
suzzingv <84590065+suzzingv@users.noreply.github.com>
2024-07-01 00:29:05 +0900

    [FEAT] user 등록

---
+++ src/main/java/konkuk/tourkk/chons/domain/user/domain/enums/SocialType.java
@@ -0,0 +1,9 @@
+package konkuk.tourkk.chons.domain.user.domain.enums;
+
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public enum SocialType {
+    GOOGLE, KAKAO, NAVER
+}
