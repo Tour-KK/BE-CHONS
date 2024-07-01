9572108f6b8c1353a7b225654467664573010e00
suzzingv <84590065+suzzingv@users.noreply.github.com>
2024-07-01 15:48:38 +0900

    [FIX] 로그인 응답에 user id 추가

--- src/main/java/konkuk/tourkk/chons/global/auth/presentation/dto/res/LoginResponse.java
+++ src/main/java/konkuk/tourkk/chons/global/auth/presentation/dto/res/LoginResponse.java
@@ -9,11 +9,13 @@ public class LoginResponse {
 
     private String accessToken;
     private String refreshToken;
+    private Long userId;
     private String email;
     private boolean isJoined;
 
-    public static LoginResponse of(String accessToken, String refreshToken, String email, boolean isJoined) {
+    public static LoginResponse of(Long userId, String accessToken, String refreshToken, String email, boolean isJoined) {
         return LoginResponse.builder()
+            .userId(userId)
             .accessToken(accessToken)
             .refreshToken(refreshToken)
             .email(email)
