032023e49a78a86beb512d84ae3bce34a641c199
suzzingv <84590065+suzzingv@users.noreply.github.com>
2024-07-02 17:32:20 +0900

    [STYLE] 코드 정리

--- src/main/java/konkuk/tourkk/chons/domain/review/infrastructure/ReviewRepository.java
+++ src/main/java/konkuk/tourkk/chons/domain/review/infrastructure/ReviewRepository.java
@@ -1,11 +1,8 @@
 package konkuk.tourkk.chons.domain.review.infrastructure;
 
-import java.util.Arrays;
 import java.util.List;
-import java.util.Optional;
 import konkuk.tourkk.chons.domain.review.domain.entity.Review;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 
 public interface ReviewRepository extends JpaRepository<Review, Long> {
 
--- src/main/java/konkuk/tourkk/chons/domain/review/presentation/controller/ReviewController.java
+++ src/main/java/konkuk/tourkk/chons/domain/review/presentation/controller/ReviewController.java
@@ -10,7 +10,6 @@ import konkuk.tourkk.chons.domain.user.domain.entity.User;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.core.parameters.P;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
