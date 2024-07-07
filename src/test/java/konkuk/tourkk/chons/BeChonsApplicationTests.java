72d41a749d77c315d80930e1b8842bdd50b71fd3
suzzingv <sjlim1999@naver.com>
2024-07-08 01:00:43 +0900

    [CI] test

--- src/test/java/konkuk/tourkk/chons/BeChonsApplicationTests.java
+++ src/test/java/konkuk/tourkk/chons/BeChonsApplicationTests.java
@@ -3,11 +3,11 @@ package konkuk.tourkk.chons;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class BeChonsApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
-
-}
+//@SpringBootTest
+//class BeChonsApplicationTests {
+//
+//    @Test
+//    void contextLoads() {
+//    }
+//
+//}
