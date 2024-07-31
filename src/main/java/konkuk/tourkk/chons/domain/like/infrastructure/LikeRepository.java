2176e0bdc49a6968b6f2274167456ba9f3463c78
suzzingv <sjlim1999@naver.com>
2024-07-31 18:52:29 +0900

    [FIX] 좋아요한 집목록 조회 리턴값 수정

--- src/main/java/konkuk/tourkk/chons/domain/like/infrastructure/LikeRepository.java
+++ src/main/java/konkuk/tourkk/chons/domain/like/infrastructure/LikeRepository.java
@@ -3,11 +3,12 @@ package konkuk.tourkk.chons.domain.like.infrastructure;
 import konkuk.tourkk.chons.domain.like.domain.entity.Like;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface LikeRepository extends JpaRepository<Like, Long> {
 
     Optional<Like> findByUserIdAndHouseId(Long userId, Long houseId);
 
-    Optional<Like> findByUserId(Long userId);
+    List<Like> findByUserId(Long userId);
 }
