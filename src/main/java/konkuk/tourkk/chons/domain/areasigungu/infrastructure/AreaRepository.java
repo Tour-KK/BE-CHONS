5dfec2c96334898f937f204d102485c6fc3efca0
suzzingv <sjlim1999@naver.com>
2024-08-18 18:07:40 +0900

    [FIX] area 이름으로 찾기 수정

--- src/main/java/konkuk/tourkk/chons/domain/areasigungu/infrastructure/AreaRepository.java
+++ src/main/java/konkuk/tourkk/chons/domain/areasigungu/infrastructure/AreaRepository.java
@@ -2,10 +2,12 @@ package konkuk.tourkk.chons.domain.areasigungu.infrastructure;
 
 import konkuk.tourkk.chons.domain.areasigungu.domain.entity.Area;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.Optional;
 
 public interface AreaRepository extends JpaRepository<Area, Long> {
 
+    @Query("select a from Area a where a.name like concat('%', :name, '%')")
     Optional<Area> findByName(String name);
 }
